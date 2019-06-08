61e833c44910633ed8f59ddaf20870272c34ca79
aditya_shivashankar <aditya.shivashankar@gmail.com>
2019-06-08 11:28:56 -0500

    Sound.java after successful run through intellij

---
+++ src/Chapter2/Sound.java
@@ -0,0 +1,10 @@
+package Chapter2;
+
+public class Sound {
+    public static void main(String[] args) {
+        double dist;
+        dist = 7.2 * 1100;
+        System.out.println("The lightning strike is " + dist + " feet away.");
+        System.out.println("The lightning strike is " + dist/2 + " feet away assuming echo.");
+    }
+}
