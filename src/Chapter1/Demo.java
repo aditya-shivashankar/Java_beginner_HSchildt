66a7b14b6101fe4b4c266595de87131666c0fd5f
aditya_shivashankar <aditya.shivashankar@gmail.com>
2020-02-19 16:48:21 -0600

    sample code to test serialization backwards compatibility

--- src/Chapter1/Demo.java
+++ src/Chapter1/Demo.java
@@ -8,6 +8,7 @@ class Demo implements java.io.Serializable
 {
     public int a;
     public String b;
+    //comment below line
     public Boolean c;
 
     // Default constructor
@@ -18,79 +19,22 @@ class Demo implements java.io.Serializable
         this.c = c;
 
     }
+    public Demo(int a, String b)
+    {
+        this.a = a;
+        this.b = b;
+    }
 
 }
 
 class DemoSub extends Demo{
     private static final long serialVersionUID = 8164894761367544387L;
+    // Boolean c;
     public DemoSub(int a, String b, Boolean c){
         super(a,b,c);
+        // super(a,b);
+        //this.c = c;
     }
 }
 
-class Test
-{
-    public static void main(String[] args)
-    {
-//        Demo object = new DemoSub(1, "geeksforgeeks", false);
-        String filename = "file.ser";
-
-        // Serialization
-//        try
-//        {
-//            //Saving of object in a file
-//            FileOutputStream file = new FileOutputStream(filename);
-//            ObjectOutputStream out = new ObjectOutputStream(file);
-//
-//            // Method for serialization of object
-//            out.writeObject(object);
-//
-//            out.close();
-//            file.close();
-//
-//            System.out.println("Object has been serialized");
-//
-//        }
-//
-//        catch(IOException ex)
-//        {
-//            System.out.println("IOException is caught");
-//        }
-
-
-        Demo object1 = null;
-
-        // Deserialization
-        try
-        {
-            // Reading the object from a file
-            FileInputStream file = new FileInputStream(filename);
-            ObjectInputStream in = new ObjectInputStream(file);
-
-
-            // Method for deserialization of object
-            object1 = (Demo)in.readObject();
-
-            in.close();
-            file.close();
-
-            System.out.println("Object has been deserialized ");
-            System.out.println("a = " + object1.a);
-            System.out.println("b = " + object1.b);
-            System.out.println("c = " + object1.c);
-        }
-
-        catch(IOException ex)
-        {
-            System.out.println("IOException is caught");
-            ex.printStackTrace();
-        }
-
-        catch(ClassNotFoundException ex)
-        {
-            System.out.println("ClassNotFoundException is caught");
-        }
-
-    }
-}
 
